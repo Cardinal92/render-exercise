bccefaa3c954aa214f5be72d108779c85466523f
Martin <rioshernanmartin@gmail.com>
2024-04-11 18:58:22 -0300

    fullsexdeveritas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(express.static("dist"))
 
 morgan.token("body", (req) => JSON.stringify(req.body))
 
@@ -25,7 +26,7 @@ let persons = [
     number: "12-43-234345"
   },
   { 
-    id: 5,
+    id: 4,
     name: "Mary Poppendieck", 
     number: "39-23-6423122"
   }
